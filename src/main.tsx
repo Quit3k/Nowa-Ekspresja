fbe8b0fb1050002b4ca0f299ef5acc7513b97692
Eryk <erykkokoczkaa@gmail.com>
2025-10-30 09:51:56 +0100

    First commit: Inicjalizacja proje

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// 1. Importujemy BrowserRouter
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* 2. "Owinęliśmy" <App /> w <BrowserRouter> */}
-    <BrowserRouter>
+    
+    {/* POPRAWKA: Dodajemy `basename` pasujący DOKŁADNIE 
+      do wartości 'base' z pliku vite.config.ts 
+    */}
+    <BrowserRouter basename="/Nowa-Ekspresja/">
       <App />
     </BrowserRouter>
+
   </React.StrictMode>,
 )
